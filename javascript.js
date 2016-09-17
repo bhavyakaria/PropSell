a225ff621c5f7eb8d37dc7fd427d109b0055b4b3
bhavyakaria <bhavyakaria@yahoo.in>
2016-09-17 18:58:26 +0530

    Removing files.

--- javascript.js
+++
@@ -1,6 +0,0 @@
-function hide(a) {
-        if (a == 1)
-            document.getElementById('register-form').style.display = 'block';
-        else
-            document.getElementById('login-form').style.display = 'none';  
-    }
