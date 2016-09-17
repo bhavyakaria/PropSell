8190d34d43d48dd5dd25249927948e70517817f8
bhavyakaria <bhavyakaria@yahoo.in>
2016-09-17 18:54:34 +0530

    Arranging Files.

---
+++ Javascript/javascript.js
@@ -0,0 +1,6 @@
+function hide(a) {
+        if (a == 1)
+            document.getElementById('register-form').style.display = 'block';
+        else
+            document.getElementById('login-form').style.display = 'none';  
+    }
